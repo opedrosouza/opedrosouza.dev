a80253a60d6f6ea6896db766bec5cb2ee0293a33
Pedro Souza <opedrosouza@hotmail.com>
2023-01-17 21:59:55 -0300

    adding gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import NextNProgress from 'nextjs-progressbar'
 import Layout from '../components/Layout'
 import SiteHead from '../components/SiteHead'
+import * as gtag from '../lib/gtag';
+import Analytics from '../components/Analytics'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Layout>
       <NextNProgress
@@ -15,6 +31,7 @@ export default function App({ Component, pageProps }: AppProps) {
       />
       <SiteHead />
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
